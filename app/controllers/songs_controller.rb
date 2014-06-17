2841786ff987f6ebd0dd2569fd8cad16346c2cc6
BYWallace <bwallace90@gmail.com>
2014-06-17 12:07:29 -0400

    working on find or create logic

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
 
   def index
-    @songs = Room.find(params[:room_id]).order("upvotes DESC").songs
+    @songs = Room.find(params[:room_id]).songs
     render json: @songs
   end
 
