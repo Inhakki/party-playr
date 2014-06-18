00b133861c6084b6838d2dc3c53ee2e8b9a72eaa
joellastraley <jstraley@gmail.com>
2014-06-18 14:58:54 -0400

    adding some drawers to the room show page

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -1,5 +1,20 @@
-$( document ).ready(function() {
-  $( "#graveyard" ).on('click', function() {
-    $( "#already-played-songs" ).slideToggle();
+$( function() {
+      var scroll_to = $("#already-played-songs").offset().top + $("#already-played-songs").height() + 200;
+
+    $( "#graveyard" ).on("click", function() {
+      $( "#already-played-songs" ).slideToggle();
+       $( "body" ).animate({
+           scrollTop: scroll_to
+       }, 1000);
+    });
+
+  $('.move').on('click', function(){
+    var el = $(this);
+    $('.slide-out-div').toggle('slide', {direction: 'left'}, 200);
+    el.text() == el.data("text-swap") ? el.text(el.data("text-original")) : el.text(el.data("text-swap"));
   });
+
 });
+
+
+
